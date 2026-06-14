6dfb8c60644c4bd6b1131be5c77e3f26c7a784e1
Daniel Prilik <danielprilik@gmail.com>
2026-06-14 16:15:13 -0400

    lunch_money: implement all missing fields on schemas

--- lunch_money/src/lib.rs
+++ lunch_money/src/lib.rs
@@ -419,6 +419,108 @@ pub mod schema {
         pub transactions: Vec<Transaction<T, E>>,
     }
 
+    /// Source of a Lunch Money transaction.
+    #[derive(Serialize, Deserialize, Clone, Copy, Debug, PartialEq, Eq)]
+    #[serde(rename_all = "lowercase")]
+    pub enum TransactionSource {
+        /// Added via the POST /transactions API
+        Api,
+        /// Added via a CSV Import
+        Csv,
+        /// Created via "Add to Cash" button on Transactions page
+        Manual,
+        /// Originally in an account that was merged
+        Merge,
+        /// Came from Plaid financial institution sync
+        Plaid,
+        /// Created from Recurring page
+        Recurring,
+        /// Created by a split rule
+        Rule,
+        /// Created by splitting another transaction
+        Split,
+        /// Legacy value replaced by csv or manual
+        User,
+    }
+
+    /// An attachment associated with a Lunch Money transaction.
+    #[derive(Serialize, Deserialize, Clone, Debug)]
+    pub struct TransactionAttachment {
+        /// The unique identifier of the attachment.
+        pub id: u64,
+        /// The id of the user who uploaded the attachment.
+        pub uploaded_by: u64,
+        /// The name of the file.
+        pub name: String,
+        /// The MIME type of the file.
+        #[serde(rename = "type")]
+        pub mime_type: String,
+        /// The size of the file in kilobytes.
+        pub size: u64,
+        /// Optional notes about the attachment.
+        pub notes: Option<String>,
+        /// The date and time when the attachment was created.
+        pub created_at: jiff::Timestamp,
+    }
+
+    /// A Lunch Money transaction that is a child of a split or group.
+    #[derive(Deserialize, Clone, Debug)]
+    pub struct ChildTransaction<T = (), E = String> {
+        /// System-created unique identifier for the transaction.
+        pub id: u64,
+        /// Date of the transaction.
+        pub date: jiff::civil::Date,
+        /// Amount of the transaction. Positive values indicate a debit, negative indicate a credit.
+        pub amount: Decimal,
+        /// Currency of the transaction.
+        pub currency: Currency,
+        /// The amount converted to the user's primary currency.
+        pub to_base: Decimal,
+        /// Payee name.
+        pub payee: String,
+        /// Original payee name from the source.
+        pub original_name: Option<String>,
+        /// Unique identifier of the associated category.
+        pub category_id: Option<u64>,
+        /// Any notes associated with the transaction.
+        pub notes: Option<String>,
+        /// Status of the transaction (e.g. reviewed, unreviewed).
+        pub status: TransactionStatus,
+        /// Denotes if the transaction is pending.
+        pub is_pending: bool,
+        /// The date and time of when the transaction was created.
+        pub created_at: jiff::Timestamp,
+        /// The date and time of when the transaction was last updated.
+        pub updated_at: jiff::Timestamp,
+        /// Denotes whether this transaction is the parent of a split.
+        pub is_split_parent: bool,
+        /// A transaction ID if this is a split transaction.
+        pub split_parent_id: Option<u64>,
+        /// Denotes whether this transaction represents a group of transactions.
+        pub is_group_parent: bool,
+        /// Denotes the ID of the group parent transaction if this is grouped.
+        pub group_parent_id: Option<u64>,
+        /// Associated manual account ID.
+        pub manual_account_id: Option<u64>,
+        /// The unique identifier of the plaid account associated with this transaction.
+        pub plaid_account_id: Option<u64>,
+        /// A list of tag_ids for the tags associated with this transaction.
+        pub tag_ids: Vec<u64>,
+        /// Source of the transaction.
+        pub source: Option<TransactionSource>,
+        /// Optional user-defined external ID.
+        pub external_id: Option<E>,
+        /// System set metadata from Plaid sync.
+        pub plaid_metadata: Option<serde_json::Value>,
+        /// Optional custom JSON metadata.
+        pub custom_metadata: Option<T>,
+        /// A list of objects that describe any attachments to the transaction.
+        #[serde(default = "Vec::new")]
+        pub files: Vec<TransactionAttachment>,
+        /// Unique identifier for associated recurring item.
+        pub recurring_id: Option<u64>,
+    }
+
     /// A Lunch Money transaction.
     #[derive(Deserialize, Clone, Debug)]
     pub struct Transaction<T = (), E = String> {
@@ -430,24 +532,54 @@ pub mod schema {
         pub amount: Decimal,
         /// Currency of the transaction.
         pub currency: Currency,
+        /// The amount converted to the user's primary currency.
+        pub to_base: Decimal,
         /// Payee name.
         pub payee: String,
+        /// Original payee name from the source.
+        pub original_name: Option<String>,
         /// Any notes associated with the transaction.
         pub notes: Option<String>,
         /// Optional user-defined external ID.
         pub external_id: Option<E>,
         /// Associated manual account ID.
         pub manual_account_id: Option<u64>,
-        /// Denotes whether this transaction is the parent of a split.
-        pub is_split_parent: Option<bool>,
-        /// Denotes the ID of the group parent transaction if this is grouped.
-        pub group_parent_id: Option<u64>,
+        /// The unique identifier of the plaid account associated with this transaction.
+        pub plaid_account_id: Option<u64>,
+        /// A list of tag_ids for the tags associated with this transaction.
+        pub tag_ids: Vec<u64>,
         /// Status of the transaction (e.g. reviewed, unreviewed).
         pub status: TransactionStatus,
+        /// Denotes if the transaction is pending.
+        pub is_pending: bool,
+        /// The date and time of when the transaction was created.
+        pub created_at: jiff::Timestamp,
+        /// The date and time of when the transaction was last updated.
+        pub updated_at: jiff::Timestamp,
+        /// Denotes whether this transaction is the parent of a split.
+        pub is_split_parent: Option<bool>,
+        /// A transaction ID if this is a split transaction.
+        pub split_parent_id: Option<u64>,
+        /// Denotes whether this transaction represents a group of transactions.
+        pub is_group_parent: bool,
+        /// Denotes the ID of the group parent transaction if this is grouped.
+        pub group_parent_id: Option<u64>,
         /// Unique identifier of the associated category.
         pub category_id: Option<u64>,
+        /// Exists only for transactions which are the parent of a split transaction or for transaction groups.
+        #[serde(default = "Vec::new")]
+        pub children: Vec<ChildTransaction<T, E>>,
+        /// System set metadata from Plaid sync.
+        pub plaid_metadata: Option<serde_json::Value>,
         /// Optional custom JSON metadata.
         pub custom_metadata: Option<T>,
+        /// A list of objects that describe any attachments to the transaction.
+        #[serde(default = "Vec::new")]
+        pub files: Vec<TransactionAttachment>,
+        /// Source of the transaction.
+        pub source: Option<TransactionSource>,
+        /// Unique identifier for associated recurring item.
+        pub recurring_id: Option<u64>,
     }
 
     /// Request payload for inserting new transactions.
@@ -470,6 +602,9 @@ pub mod schema {
         /// Payee name.
         #[serde(skip_serializing_if = "Option::is_none")]
         pub payee: Option<String>,
+        /// Original payee name.
+        #[serde(skip_serializing_if = "Option::is_none")]
+        pub original_name: Option<String>,
         /// Transaction notes.
         #[serde(skip_serializing_if = "Option::is_none")]
         pub notes: Option<String>,
@@ -479,6 +614,12 @@ pub mod schema {
         /// Unique identifier for the manually managed account.
         #[serde(skip_serializing_if = "Option::is_none")]
         pub manual_account_id: Option<u64>,
+        /// The unique identifier of the plaid account associated with this transaction.
+        #[serde(skip_serializing_if = "Option::is_none")]
+        pub plaid_account_id: Option<u64>,
+        /// Unique identifier for associated recurring item.
+        #[serde(skip_serializing_if = "Option::is_none")]
+        pub recurring_id: Option<u64>,
         /// Transaction status (reviewed or unreviewed).
         #[serde(skip_serializing_if = "Option::is_none")]
         pub status: Option<TransactionStatus>,
@@ -507,8 +648,20 @@ pub mod schema {
         pub id: u64,
         /// Name of the tag.
         pub name: String,
+        /// Description of the tag.
+        pub description: Option<String>,
+        /// The text color of the tag.
+        pub text_color: Option<String>,
+        /// The background color of the tag.
+        pub background_color: Option<String>,
+        /// The date and time of when the tag was created.
+        pub created_at: jiff::Timestamp,
+        /// The date and time of when the tag was last updated.
+        pub updated_at: jiff::Timestamp,
         /// If true, the tag is archived and hidden in the app UI.
         pub archived: bool,
+        /// The date and time of when the tag was last archived.
+        pub archived_at: Option<jiff::Timestamp>,
     }
 
     /// Request payload for creating a new tag.
@@ -545,12 +698,24 @@ pub mod schema {
         /// Payee name.
         #[serde(skip_serializing_if = "Option::is_none")]
         pub payee: Option<String>,
+        /// Unique identifier of the category for this transaction. Set to null to clear.
+        #[serde(skip_serializing_if = "Option::is_none")]
+        #[expect(clippy::option_option)]
+        pub category_id: Option<Option<u64>>,
         /// Transaction notes.
         #[serde(skip_serializing_if = "Option::is_none")]
         pub notes: Option<String>,
-        /// Optional custom JSON metadata.
+        /// The unique identifier of the manual account associated with this transaction. Set to null to clear.
         #[serde(skip_serializing_if = "Option::is_none")]
-        pub custom_metadata: Option<T>,
+        #[expect(clippy::option_option)]
+        pub manual_account_id: Option<Option<u64>>,
+        /// The unique identifier of the plaid account associated with this transaction. Set to null to clear.
+        #[serde(skip_serializing_if = "Option::is_none")]
+        #[expect(clippy::option_option)]
+        pub plaid_account_id: Option<Option<u64>>,
+        /// A list of tag_ids for the tags associated with this transaction. If set, overwrites existing tags.
+        #[serde(skip_serializing_if = "Option::is_none")]
+        pub tag_ids: Option<Vec<u64>>,
         /// Optional list of tag IDs to add.
         #[serde(skip_serializing_if = "Option::is_none")]
         pub additional_tag_ids: Option<Vec<u64>>,
@@ -558,6 +723,12 @@ pub mod schema {
         #[serde(skip_serializing_if = "Option::is_none")]
         #[expect(clippy::option_option)]
         pub external_id: Option<Option<E>>,
+        /// Optional custom JSON metadata.
+        #[serde(skip_serializing_if = "Option::is_none")]
+        pub custom_metadata: Option<T>,
+        /// Status of the transaction (reviewed or unreviewed).
+        #[serde(skip_serializing_if = "Option::is_none")]
+        pub status: Option<TransactionStatus>,
     }
 
     /// Response payload returned by a transaction insertion request.
@@ -608,14 +779,32 @@ pub mod schema {
         pub id: u64,
         /// Name of the category.
         pub name: String,
-        /// Whether this category is a group containing other categories.
-        pub is_group: bool,
+        /// The description of the category or `null` if not set.
+        pub description: Option<String>,
+        /// If true, the transactions in this category will be treated as income.
+        pub is_income: bool,
+        /// If true, the transactions in this category will be excluded from the budget.
+        pub exclude_from_budget: bool,
+        /// If true, the transactions in this category will be excluded from totals.
+        pub exclude_from_totals: bool,
+        /// The date and time of when the category was last updated.
+        pub updated_at: jiff::Timestamp,
+        /// The date and time of when the category was created.
+        pub created_at: jiff::Timestamp,
         /// ID of the parent category group, if applicable.
         pub group_id: Option<u64>,
-        /// Whether this category is archived.
-        pub archived: bool,
+        /// Whether this category is a group containing other categories.
+        pub is_group: bool,
         /// Optional list of children categories (only populated for groups).
         pub children: Option<Vec<ChildCategory>>,
+        /// Whether this category is archived.
+        pub archived: bool,
+        /// The date and time of when the category was last archived.
+        pub archived_at: Option<jiff::Timestamp>,
+        /// An integer specifying the position in which the category is displayed.
+        pub order: Option<u32>,
+        /// If true, the category is collapsed in the Lunch Money GUI.
+        pub collapsed: bool,
     }
 
     /// A category that is a child of a category group.
@@ -625,31 +814,71 @@ pub mod schema {
         pub id: u64,
         /// Name of the category.
         pub name: String,
+        /// The description of the category or `null` if not set.
+        pub description: Option<String>,
+        /// If true, the transactions in this category will be treated as income.
+        pub is_income: bool,
+        /// If true, the transactions in this category will be excluded from the budget.
+        pub exclude_from_budget: bool,
+        /// If true, the transactions in this category will be excluded from totals.
+        pub exclude_from_totals: bool,
+        /// The date and time of when the category was last updated.
+        pub updated_at: jiff::Timestamp,
+        /// The date and time of when the category was created.
+        pub created_at: jiff::Timestamp,
         /// ID of the parent category group.
         pub group_id: Option<u64>,
         /// Whether this category is archived.
         pub archived: bool,
+        /// The date and time of when the category was last archived.
+        pub archived_at: Option<jiff::Timestamp>,
+        /// An index specifying the position in which the category is displayed.
+        pub order: Option<u32>,
+        /// If true, the category is collapsed in the Lunch Money GUI.
+        pub collapsed: Option<bool>,
     }
 
     /// A manually managed account in Lunch Money.
     #[derive(serde::Deserialize, Clone, Debug)]
-    pub struct ManualAccount {
+    pub struct ManualAccount<E = String> {
         /// Unique identifier of the manual account.
         pub id: u64,
         /// Name of the manual account.
         pub name: String,
+        /// Name of institution holding the account.
+        pub institution_name: Option<String>,
         /// Optional display name.
         pub display_name: Option<String>,
         /// Primary type of the account.
         #[serde(rename = "type")]
         pub account_type: AccountType,
+        /// Optional account subtype.
+        pub subtype: Option<String>,
         /// Current balance of the manual account.
         #[serde(with = "rust_decimal::serde::str")]
         pub balance: Decimal,
         /// Currency of the manual account balance.
         pub currency: Currency,
+        /// The balance converted to the user's primary currency.
+        pub to_base: Decimal,
+        /// Date balance was last updated.
+        pub balance_as_of: String,
         /// Account status (active or closed).
         pub status: AccountStatus,
+        /// The date this account was closed.
+        pub closed_on: Option<jiff::civil::Date>,
+        /// An optional external ID.
+        pub external_id: Option<E>,
+        /// User defined JSON data.
+        pub custom_metadata: Option<serde_json::Value>,
+        /// If true, this account will not show up for assignment.
+        pub exclude_from_transactions: bool,
+        /// The name of the user who created the account.
+        pub created_by_name: String,
+        /// Date/time the account was created.
+        pub created_at: jiff::Timestamp,
+        /// Date/time the account was last updated.
+        pub updated_at: jiff::Timestamp,
     }
 
     /// Response payload containing a list of manual accounts.
